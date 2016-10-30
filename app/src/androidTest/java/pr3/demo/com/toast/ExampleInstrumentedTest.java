9a51f6c199ef12b847c9f4393587afa4cc44c5bc
gdmec07150648 <497515439@qq.com>
2016-10-30 21:13:13 +0800

    Initial commit

---
+++ app/src/androidTest/java/pr3/demo/com/toast/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package pr3.demo.com.toast;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("pr3.demo.com.toast", appContext.getPackageName());
+    }
+}
